e48150c7d857a745c79dedddd1c31539537dbff7
codeconcept <contact@codeconcept.fr>
2019-06-11 13:41:51 +0200

    ExpenseTotal component that subscribes to store to update total on every new expense

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,24 @@
 <script>
-import ExpenseTable from './components/ExpenseTable.svelte';
-import ExpenseCreate from './components/ExpenseCreate.svelte';
-	export let title;
-	import store from './shared/ExpenseStore.js';
-	
-	let expenses = [];
-	store.subscribe(data => {
-		expenses = data;
-	});
-  
+  import ExpenseTable from "./components/ExpenseTable.svelte";
+  import ExpenseCreate from "./components/ExpenseCreate.svelte";
+  import ExpenseTotal from "./components/ExpenseTotal.svelte";
+  export let title;
+  import store from "./shared/ExpenseStore.js";
 
+  let expenses = [];
+  store.subscribe(data => {
+    expenses = data;
+  });
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
 <h1>{title}</h1>
 
-<ExpenseCreate/>
-<ExpenseTable expenses={expenses}/>
+<ExpenseTotal />
+<ExpenseCreate />
+<ExpenseTable {expenses} />
